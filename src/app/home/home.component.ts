d40d327f0e0c824b560d5f37b48620251294c7ab
Kristina Baskakova <kristina.baskakova15@mail.ru>
2019-08-20 11:33:32 +0300

    1 commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public filePath;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  fileChange(event) {
+    this.filePath = event.target.result; 
+    let fileList: FileList = event.target.files;
+    if(fileList.length > 0) {
+        let file: File = fileList[0];
+        let formData:FormData = new FormData();
+        formData.append('uploadFile', file, file.name);
+    }
+}
 
 }
