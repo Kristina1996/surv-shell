89cc06c91b2cded9e22702333a072dbad7fa3446
Kristina1996 <kristina.baskakova15@mail.ru>
2019-10-03 10:52:01 +0300

    Добавлены два Input (color, font-size) для компонента IconButton #28

--- src/app/shared/components/icon-button/icon-button.component.ts
+++ src/app/shared/components/icon-button/icon-button.component.ts
@@ -2,16 +2,19 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-icon-button',
-  template: `<i class="material-icons icon-button" title="{{title}}">{{iconType}}</i>`,
+  template: `<i [ngClass]="[color, size]" class="material-icons icon-button" title="{{title}}">{{iconType}}</i>`,
   styleUrls: ['./icon-button.component.scss']
 })
 export class IconButtonComponent implements OnInit {
 
   @Input() iconType: string;
   @Input() title: string;
+  @Input() color = 'light';
+  @Input() size = 'size-normal';
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
