f679a00ef2f917ce2025e5b1f8bbf83dccfa3daa
Kristina1996 <kristina.baskakova15@mail.ru>
2019-10-08 14:52:45 +0300

    Изменен тип инпута с количеством часов на number #37
    Добавлено дополнительное свойство type (по умолчанию значение: text) компоненту инпута

--- src/app/shared/components/report/form-input/form-input.component.ts
+++ src/app/shared/components/report/form-input/form-input.component.ts
@@ -3,13 +3,14 @@ import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-form-input',
-  template: `<input placeholder="{{placeHolder}}" [formControl]="control">`,
+  template: `<input placeholder="{{placeHolder}}" [formControl]="control" type="{{type}}">`,
   styleUrls: ['./form-input.component.scss']
 })
 export class FormInputComponent implements OnInit {
 
   @Input() control: FormControl;
   @Input() placeHolder: string;
+  @Input() type = 'text';
 
   constructor() { }
 
